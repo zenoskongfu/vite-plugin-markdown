06127cd8ac26bfe45ba8152ca18cd7fbb4dd9545
zenoskongfu <zenos.chen@foxmail.com>
2025-02-20 23:34:30 +0800

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import Readme from "../README.md?raw";
-import { remark } from "remark";
-import remarkRehype from "remark-rehype";
-import rehypeRaw from "rehype-raw";
-import rehypeReact from "rehype-react";
-import rehypeStringify from "rehype-stringify";
-import MarkdownReact from "./component/MarkdownReact";
-import getCodeblock from "./util/getCodeblock";
-import FoomdStr from "./docs/Foo.md?raw";
 import Foomd from "./docs/Foo.md";
-
-import markdown2html from "./util/markdown2html";
-import EditCode from "./component/EditCode";
-import FromInput from "./component/FormInput";
 import DynamicCompo from "./component/DynamicCompo";
 
 function App() {
-  const [content, setContent] = useState("");
-  const [compo, setCompo] = useState<any>();
-
-  useEffect(() => {
-    // getCodeblock(FoomdStr).then((res) => {
-    //   console.log("code block: ", res);
-    // });
-  }, []);
   return (
     <div>
-      {/* <DynamicCompo /> */}
+      <DynamicCompo />
       {/* <MarkdownReact code={FoomdStr} /> */}
       <Foomd />
       {/* <EditCode
